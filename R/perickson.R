a8cef6b8323567c2623819cdf9fd3d06e9405dd7
Fraenzi Korner-Nievergelt <fraenzi.korner@oikostat.ch>
2013-09-05 08:37:30 +0000

    version 1.1

--- R/perickson.R
+++ R/perickson.R
@@ -1,5 +1,8 @@
 perickson <-
-function(t_quer, f, d){
-(t_quer*f/d*((exp(d/t_quer)-1)/(exp(d/t_quer)-1+f))) 
+function(t.bar=NULL, s, f, d){
+if(is.null(t.bar)) t.bar <- 1/(-log(s)) 
+(t.bar*f/d*((exp(d/t.bar)-1)/(exp(d/t.bar)-1+f))) 
 }
 
+# changes:
+# 3. 9. 2013: argument "t_quer" was renamed into "t.bar" and possibility to use s instead of tbar added
--- R/pkorner.R
+++ R/pkorner.R
@@ -1,21 +1,67 @@
+# Log of changes:
+# - FK, 23.06.2013: Bug when using search.efficiency.constant=FALSE removed.
+# - TR, 26.06.2013: Function can also handle non-constant persistence probability.
+# - FK, 26.08.2013: fixed the problem that function returned NA when for non -constant persistence probabilities less number of estimates than n*d were available. The function now assumes that the proportion of persistant carcasses is zero after the last estimate. 
+
 pkorner <-
-function(s, f, d, n, k=0.25, search.efficiency.constant=TRUE){
-# s = probability that a carcass remains 24 hours
-# f = probability that a carcass is detected by a 
-# searcher during a search given it persisted to the search
-# d = (average) number of days between two searches
-# n = number of searches (n * d = length of study period)
-#--------------------------------------------------------------
-if(search.efficiency.constant){
-x <- (1-f)*s^d
-A <- s*(1-s^d)/(1-s)
-summep <- numeric(n)
-for(k in 0:(n-1)) summep[k+1] <- (n-k)*x^k
-p <- A*f*sum(summep)/(d*n)
-}
-if(!search.efficiency.constant){
-p <- pkorner.version2(s=s, f=f, d=d, n=n, k=k)
-}
-p
+function(s, s.lower=NA, s.upper=NA, f, f.lower=NA, f.upper=NA, d, n, k=0.25, search.efficiency.constant=TRUE, CI=FALSE, nsim=1000){
+  # s = probability that a carcass remains 24 hours (scalar if constant, vector otherwise)
+  # f = probability that a carcass is detected by a searcher during a search given it persisted to the search
+  # d = (average) number of days between two searches
+  # n = number of searches (n * d = length of study period)
+  # k = factor by which the search efficiency is multiblied after each search
+  #--------------------------------------------------------------
+  persistence.probability.constant <- length(s)==1
+  if(search.efficiency.constant & persistence.probability.constant){
+    x <- (1-f)*s^d
+    A <- s*(1-s^d)/(1-s)
+    summep <- numeric(n)
+    for(i in 0:(n-1)) summep[i+1] <- (n-i)*x^i
+    p <- A*f*sum(summep)/(d*n)
+  }
+  if(!search.efficiency.constant & persistence.probability.constant){
+    A<-s*(1-s^d)/(1-s)
+    summepfound<-A*f
+    for(x in 2:n){
+      summep<-1
+      for(j in 1:(x-1)) summep<-summep+k^(x-j) * s^((x-j)*d) * productsef(f, k, n=x, j)
+      summepfound<-summepfound+A*f*summep
+    }
+    p<-summepfound/(d*n)     
+  }
+  if(!persistence.probability.constant){
+    f <- rep(f, n)
+    if(!search.efficiency.constant) for(j in 2:length(f)) f[j] <- f[j-1]*k
+    c <- array(0, dim=c(n*d, n*d))
+    found <- array(0, dim=c(n*d, n*d))
+    diag(c) <- s[1]
+    ns <- length(s)      # fk inserted
+    
+    if(ns<10) message(paste("Proportion of persisting carcasses is only given for", ns, "days. It is assumed that no carcass persists longer than", ns, "days!"))
+    
+    for(day in 2:(n*d)) {
+      for(cohort in 1:(day-1)) c[day,cohort] <- (c[day-1,cohort] - found[day-1,cohort])*ifelse((day-cohort+1)<= ns, s[day-cohort+1], 0) # fk inserted ifelse function
+      for(cohort in 1:day) if(day%%d==0) found[day,cohort] <- c[day,cohort]*f[ceiling((day-cohort+1)/d)]
+    }
+    p <- sum(found)/(n*d)
+  }
+  
+### Get confidence intervals
+  if(CI){
+    if(is.na(s.lower[1]) | is.na(s.upper[1]) | is.na(f.lower) | is.na(f.upper)) stop("'Please provide CI for s and f.")
+    f.a <- shapeparameter(f, f.lower, f.upper)$a
+    f.b <- shapeparameter(f, f.lower, f.upper)$b
+    s.a <- shapeparameter(s, s.lower, s.upper)$a
+    s.b <- shapeparameter(s, s.lower, s.upper)$b
+    resCI <- numeric(nsim)
+    for(i in 1:nsim) {
+      resCI[i] <- pkorner(s=rbeta(length(s.a), s.a, s.b), f=rbeta(1,f.a, f.b), d=d, n=n, search.efficiency.constant=search.efficiency.constant, CI=FALSE, nsim=nsim)
+    }
+    names(p) <- "p"
+    p <- c(p, quantile(x=resCI, probs=c(0.025, 0.975)))
+  }
+  
+### Return results  
+  p
 }
 
